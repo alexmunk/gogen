b5788ab830a7c0e7b207f2190a2ec1d2d0b3b9ab
Clint Sharp <clint@typhoon.org>
2016-10-16 13:58:28 -0700

    Moving to bytes.Buffer for string appending

--- generator/sample.go
+++ generator/sample.go
@@ -2,10 +2,16 @@ package generator
 
 import (
 	"math"
-	"strings"
 
 	"github.com/coccyx/gogen/internal"
+	"github.com/oxtoacart/bpool"
 )
+
+var bp *bpool.BufferPool
+
+func init() {
+	bp = bpool.NewBufferPool(100)
+}
 
 type sample struct{}
 
@@ -38,12 +44,14 @@ func genSinglePass(item *config.GenQueueItem) error {
 
 			for i := 0; i < item.Count; i++ {
 				events = append(events, getBrokenEvent(item, item.Rand.Intn(slen)))
+				// events[i] = getBrokenEvent(item, item.Rand.Intn(slen))
 			}
 		} else {
 			if item.Count <= slen {
 				for i := 0; i < item.Count; i++ {
 					// s.Log.Debugf("Count <= sample len, filling with sample '%s' for %d events", s.Name, item.Count)
 					events = append(events, getBrokenEvent(item, i))
+					// events[i] = getBrokenEvent(item, i)
 				}
 			} else {
 				iters := int(math.Ceil(float64(item.S.Count) / float64(slen)))
@@ -60,6 +68,7 @@ func genSinglePass(item *config.GenQueueItem) error {
 					// end := (i * slen) + count
 					for j := 0; j < count; j++ {
 						events = append(events, getBrokenEvent(item, j))
+						// events[j] = getBrokenEvent(item, j)
 					}
 				}
 			}
@@ -71,14 +80,18 @@ func genSinglePass(item *config.GenQueueItem) error {
 }
 
 func getBrokenEvent(item *config.GenQueueItem, i int) map[string]string {
-	ret := make(map[string]string)
 	s := item.S
+	ret := make(map[string]string, len(s.BrokenLines[i]))
 	choices := make(map[int]*int)
 	for k, v := range s.BrokenLines[i] {
-		tokens := make([]string, len(v))
+		// tokens := make([]string, len(v))
+		// var event string
+		event := bp.Get()
 		for _, st := range v {
 			if st.T == nil {
-				tokens = append(tokens, st.S)
+				// tokens = append(tokens, st.S)
+				// event += st.S
+				event.WriteString(st.S)
 			} else {
 				var choice *int
 				if choices[st.T.Group] != nil {
@@ -91,13 +104,16 @@ func getBrokenEvent(item *config.GenQueueItem, i int) map[string]string {
 				if err != nil {
 					s.Log.Errorf("Error generating replacement for token '%s' in sample '%s'", st.T.Name, s.Name)
 				}
-				tokens = append(tokens, replacement)
+				// tokens = append(tokens, replacement)
+				// event += replacement
+				event.WriteString(replacement)
 				if st.T.Group > 0 {
 					choices[st.T.Group] = choice
 				}
 			}
 		}
-		ret[k] = strings.Join(tokens, "")
+		ret[k] = event.String()
+		bp.Put(event)
 	}
 	return ret
 }
